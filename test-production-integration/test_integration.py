3c47815f4b7183aac0e1b43f7961e80cdb588c03
Ming Wang <mwang87@gmail.com>
2019-12-29 15:03:33 -0600

    adding more functional tests

--- test-production-integration/test_integration.py
+++ test-production-integration/test_integration.py
@@ -6,3 +6,13 @@ def test_production():
     url = f"https://{PRODUCTION_URL}/heartbeat"
     r = requests.get(url)
     r.raise_for_status()
+    
+def test_keycount():
+    url = f"https://{PRODUCTION_URL}/keycount"
+    r  = requests.get(url)
+    r.raise_for_status()
+    
+def test_caffeine():
+    url = f"https://{PRODUCTION_URL}/entities/RYYVLZVUVIJVGH-UHFFFAOYSA-N.json"
+    r  = requests.get(url)
+    r.raise_for_status()
