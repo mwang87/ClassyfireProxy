eea31bf10518c94af437eea4bafadfba8cc82112
cmaceves <christineaceves22@gmail.com>
2020-04-23 14:49:21 -0700

    test_integration.py

--- test-production-integration/test_integration.py
+++ test-production-integration/test_integration.py
@@ -18,7 +18,7 @@ def test_inchi_key():
     r.raise_for_status()
 
 def test_inchi():
-    url = f"https://{PRODUCTION_URL}/entities/inchi?entity_name=1S/C8H10N4O2/c1-10-4-9-6-5(10)7(13)12(3)8(14)11(6)2/h4H,1-3H3.json"
+    url = f"https://{PRODUCTION_URL}/entities/inchi?entity_name=InChI=1S/C8H10N4O2/c1-10-4-9-6-5(10)7(13)12(3)8(14)11(6)2/h4H,1-3H3.json"
     r = requests.get(url)
     r.raise_for_status()
 
