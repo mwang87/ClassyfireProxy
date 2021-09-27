ab17bd7798e977e427c77f0ea63b91bc24258099
Mingxun Wang <mwang87@gmail.com>
2021-09-27 11:36:23 -0700

    updating testing

--- test-production-integration/test_integration.py
+++ test-production-integration/test_integration.py
@@ -2,17 +2,25 @@ import requests
 
 PRODUCTION_URL = "gnps-classyfire.ucsd.edu"
 
+def test_url(url, timeout=10):
+    try:
+        r = requests.get(url, timeout=timeout)
+        return r
+    except:
+        print("Timeout", url)
+        return None
+
 def test_production():
     url = f"https://{PRODUCTION_URL}/heartbeat"
-    r = requests.get(url)
+    r = test_url(url)
     r.raise_for_status()
     
 def test_keycount():
     url = f"https://{PRODUCTION_URL}/keycount"
-    r  = requests.get(url)
+    r  = test_url(url)
     r.raise_for_status()
     
 def test_caffeine():
     url = f"https://{PRODUCTION_URL}/entities/RYYVLZVUVIJVGH-UHFFFAOYSA-N.json"
-    r  = requests.get(url)
+    r  = test_url(url)
     r.raise_for_status()
