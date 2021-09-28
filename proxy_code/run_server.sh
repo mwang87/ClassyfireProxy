a7af41a9f8f74fe7675637a58eda1ec839be0843
Mingxun Wang <mwang87@gmail.com>
2021-09-27 23:46:27 -0700

    adding timeout

--- proxy_code/run_server.sh
+++ proxy_code/run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn -w 16 -b 0.0.0.0:5000 --timeout 3600 --max-requests 1600 --max-requests-jitter 100 main:app
+gunicorn -w 16 -b 0.0.0.0:5000 --timeout 20 --max-requests 1600 --max-requests-jitter 100 main:app
