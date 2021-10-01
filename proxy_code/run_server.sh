f61630ae0a2f3fd247d0dfc0273e399dc72a61c5
Mingxun Wang <mwang87@gmail.com>
2021-10-01 10:36:45 -0700

    limit parallelism

--- proxy_code/run_server.sh
+++ proxy_code/run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn -w 16 -b 0.0.0.0:5000 --timeout 20 --max-requests 1600 --max-requests-jitter 100 main:app
+gunicorn -w 4 --threads 4 -b 0.0.0.0:5000 --timeout 20 --max-requests 500 --max-requests-jitter 100 main:app
