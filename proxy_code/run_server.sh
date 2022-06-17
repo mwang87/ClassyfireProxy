db2c53a44c343d24d2d48e58b269f9d00ec7a028
Mingxun Wang <mwang87@gmail.com>
2022-06-17 14:05:46 -0700

    saving logs

--- proxy_code/run_server.sh
+++ proxy_code/run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn -w 4 --threads 4 -b 0.0.0.0:5000 --timeout 15 --max-requests 5000 --max-requests-jitter 100 main:app
+gunicorn -w 4 --threads=4 --worker-class=gthread -b 0.0.0.0:5000 --timeout 15 --max-requests 5000 --max-requests-jitter 100 --graceful-timeout 15 main:app --access-logfile /app/logs/access.log
