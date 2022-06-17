781164bb95a7fe5ff79704ca11cfbbe2682603be
Mingxun Wang <mwang87@gmail.com>
2022-06-17 09:45:28 -0700

    updating restart

--- proxy_code/run_server.sh
+++ proxy_code/run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn -w 4 --threads 4 -b 0.0.0.0:5000 --timeout 15 --max-requests 500 --max-requests-jitter 100 main:app
+gunicorn -w 4 --threads 4 -b 0.0.0.0:5000 --timeout 15 --max-requests 5000 --max-requests-jitter 100 main:app
